c8bebbd611d1f67cdf138ee90aa41690de14f1ef
davidabutler92 <davidabutler92@yahoo.com>
2020-10-13 15:26:29 -0700

    add pokemon to pokeCart and imcriment encountered and caught

--- helper-functions.js
+++ helper-functions.js
@@ -1,4 +1,15 @@
+import { pokemon } from './api.js';
+
 export function getRandomPokemon(pokemon) {
     const randomPokemon = Math.floor(Math.random() * pokemon.length);
     return pokemon[randomPokemon];
+}
+
+export function findByName(someArray, someId) {
+    for (let i = 0; i < someArray.length; i++) {
+        const item = someArray[i];
+        if (item.pokemon === someId) {
+            return item;
+        }
+    }
 }
---
+++ pokeCart.js
@@ -0,0 +1,17 @@
+export const pokeCart = [
+    {
+        pokemon: 'charmander',
+        encountered: 1,
+        captured: 0,
+    },
+    {
+        pokemon: 'bulbasaur',
+        encountered: 1,
+        captured: 3,
+    },
+    {
+        pokemon: 'squirtle',
+        encountered: 1,
+        captured: 2,
+    }
+];
